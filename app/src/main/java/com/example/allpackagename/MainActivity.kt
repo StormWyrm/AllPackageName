8e38888997b0c34b1eeed5466044fd15bacfb0c3
liqingfeng <qingfeng-love@qq.com>
2019-03-01 17:12:57 +0800

    新增应用排列顺序选择

--- app/src/main/java/com/example/allpackagename/MainActivity.kt
+++ app/src/main/java/com/example/allpackagename/MainActivity.kt
@@ -34,7 +34,8 @@ class MainActivity : AppCompatActivity() {
     private val ALL_APP = 0x00000033
 
     private var curMode = ALL_APP
-    private var adapter: MainAdapter? = null
+    private var curSortMode = 0 //0:按应用名排序 1：按包名排序
+    private lateinit var mAdapter: MainAdapter
 
     companion object {
         var appInfos: ArrayList<AppInfo> = ArrayList()
@@ -51,14 +52,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initToolbar()
+        initView()
         loadAppinfo()
-    }
-
-    private fun initToolbar() {
-        toolbar.run {
-            title = "AllPackageName"
-            setSupportActionBar(this)
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -94,6 +89,23 @@ class MainActivity : AppCompatActivity() {
                 }
                 curMode = ALL_APP
                 loadAppinfo()
+            }
+            R.id.sort -> {
+                if (item.title.equals("按包名排序")) {
+                    appInfos.sortWith(Comparator { o1, o2 ->
+                        var num = o1.packageName.compareTo(o2.packageName)
+                        if (num == 0)
+                            num = o1.appName.compareTo(o1.appName)
+                        return@Comparator num
+                    })
+                    item.title = "按应用名排序"
+                    curSortMode = 1
+                } else {
+                    appInfos.sort()
+                    item.title = "按包名排序"
+                    curMode = 0
+                }
+                mAdapter.setNewData(appInfos)
             }
             R.id.share -> {
                 if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
@@ -137,7 +149,7 @@ class MainActivity : AppCompatActivity() {
                 file.createNewFile()
             }
             val ps = PrintStream(file)
-            val appInfos = adapter?.data ?: ArrayList<AppInfo>()
+            val appInfos = mAdapter?.data ?: ArrayList<AppInfo>()
             for (appInfo in appInfos) {
                 val msg = "应用名：${appInfo.appName}; 包名：${appInfo.packageName}; 签名: ${appInfo.signature}"
                 ps.write(msg.toByteArray())
@@ -160,32 +172,50 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
+    private fun initView() {
+        mAdapter = MainAdapter(null).apply {
+            setOnItemLongClickListener { _, _, position ->
+                getItem(position)?.run {
+                    val msg = "应用名：$appName; 包名：$packageName; 签名: $signature"
+                    val clipboardManager =
+                        this@MainActivity.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    clipboardManager.primaryClip = ClipData.newPlainText(null, msg)
+                    Toast.makeText(this@MainActivity, "已复制应用信息到剪切板！", Toast.LENGTH_SHORT).show()
+                }
+                true
+            }
+        }
+        rv.run {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = mAdapter
+        }
+    }
+
+    private fun initToolbar() {
+        toolbar.run {
+            title = "AllPackageName"
+            setSupportActionBar(this)
+        }
+    }
+
     private fun loadAppinfo() {
         dialog.show()
         Thread {
-            val appInfos = getAppInfo(curMode)
-            Collections.sort(appInfos)
+            getAppInfo(curMode)
+            if (curSortMode == 0) {
+                appInfos.sort()
+            } else {
+                appInfos.sortWith(Comparator { o1, o2 ->
+                    var num = o1.packageName.compareTo(o2.packageName)
+                    if (num == 0)
+                        num = o1.appName.compareTo(o1.appName)
+                    return@Comparator num
+                })
+            }
             runOnUiThread {
                 dialog.dismiss()
-                if (adapter == null) {
-                    adapter = MainAdapter(appInfos)?.apply {
-                        setOnItemLongClickListener { _, _, position ->
-                            getItem(position)?.run {
-                                val msg = "应用名：$appName; 包名：$packageName; 签名: $signature"
-                                val clipboardManager =
-                                    this@MainActivity.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                                clipboardManager.primaryClip = ClipData.newPlainText(null, msg)
-                                Toast.makeText(this@MainActivity, "已复制应用信息到剪切板！", Toast.LENGTH_SHORT).show()
-                            }
-                            true
-                        }
-                    }
-                    rv.layoutManager = LinearLayoutManager(this)
-                    rv.adapter = adapter
-                } else {
-                    adapter?.setNewData(appInfos)
-                }
                 setAppCount(appInfos.size)
+                mAdapter.setNewData(appInfos)
             }
         }.start()
     }
@@ -200,43 +230,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    private fun getAppInfo(appType: Int = ALL_APP): List<AppInfo> {
+    private fun getAppInfo(appType: Int = ALL_APP) {
         val packageInfos = packageManager.getInstalledPackages(PackageManager.GET_SIGNATURES)
-        val appInfo = ArrayList<AppInfo>()
+        appInfos.clear()
         for (packageInfo in packageInfos) {
             when (appType) {
                 SYSTEM_APP -> {
                     if (packageInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0) {
-                        addToList(packageInfo, appInfo)
+                        addToList(packageInfo)
                     }
                 }
                 INSTALL_APP -> {
                     if (packageInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
-                        addToList(packageInfo, appInfo)
+                        addToList(packageInfo)
                     }
                 }
                 else -> {
-                    addToList(packageInfo, appInfo)
+                    addToList(packageInfo)
                 }
             }
 
         }
-        appInfos.clear()
-        appInfos.addAll(appInfo)
-        return appInfo
 
     }
 
-    private fun addToList(
-        packageInfo: PackageInfo,
-        appInfo: ArrayList<AppInfo>
-    ) {
+    private fun addToList(packageInfo: PackageInfo) {
         val appName = packageInfo.applicationInfo.loadLabel(packageManager)
         val packageName = packageInfo.packageName
         val appIcon = packageInfo.applicationInfo.loadIcon(packageManager)
         val signature = packageInfo.signatures[0].toByteArray()
-        appInfo.add(AppInfo(appName as String, packageName, appIcon, getSignatureSha1(signature)))
+        appInfos.add(AppInfo(appName as String, packageName, appIcon, getSignatureSha1(signature)))
     }
 
     private fun getSignatureSha1(bytes: ByteArray): String {
